e1000ac038f5e5c4c339bc3cda3ace86fd72a63f
kayserjhamillex <anibaljhamil@gmail.com>
2022-04-06 22:04:30 -0500

    enlaando con el dolor

--- src/app/admin/procesos/proceso7/subproceso2de7/subproceso2de7.component.ts
+++ src/app/admin/procesos/proceso7/subproceso2de7/subproceso2de7.component.ts
@@ -8,8 +8,14 @@ import { Genero } from 'src/app/models/genero.model';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ListHorario } from 'src/app/models/listhorario';
 import { CitaService } from 'src/app/services/cita.service';
-import { ClienteService } from 'src/app/services/cliente.service';
+import { ListDetalleCita } from 'src/app/models/Listdetallecita';
 import { HorarioService } from 'src/app/services/horario.service';
+import { ClienteService } from 'src/app/services/cliente.service';
+import { ListDermatomaFrontal } from 'src/app/models/listdermatomafrontal';
+import { ListDermatomaPosterior } from 'src/app/models/listdermatomaposterior';
+import { DetallecitacitaService } from 'src/app/services/detallecitacita.service';
+import { DermatomafrontalService } from 'src/app/services/dermatomafrontal.service';
+import { DermatomaposteriorService } from 'src/app/services/dermatomaposterior.service';
 
 @Component({
   selector: 'app-subproceso2de7',
@@ -17,6 +23,146 @@ import { HorarioService } from 'src/app/services/horario.service';
   styleUrls: ['./subproceso2de7.component.css']
 })
 export class Subproceso2de7Component implements OnInit {
+  detallecita: ListDetalleCita = {
+    id: 0,
+    G: '',
+    P: '',
+    A: '',
+    C: '',
+    Fum: new Date(),
+    Papsmear: '',
+    Bloodtype: '',
+    Others: '',
+    Startdate: '',
+    Frequency: '',
+    Datethatbecameimportant: new Date(),
+    Perday: '',
+    Perweek: '',
+    Permonth: '',
+    Byyear: '',
+    One: false,
+    Two: false,
+    Three: false,
+    Multiple: false,
+    Unilateral: false,
+    Bilateral: false,
+    Symmetrical: false,
+    Referred: false,
+    Irradiated: false,
+    Worsened: false,
+    Parked: false,
+    Decreasing: false,
+    Scale: '',
+    Datescale: new Date(),
+    Oncological: false,
+    Oncologicaltype: '',
+    AssociatedwithDiagnosis: false,
+    AssociatedwithTreatment: false,
+    AssociatedwithProgressiveCancer: false,
+    Addictions: false,
+    Dying: false,
+    Treatment: '',
+    Observations: '',
+    Protocol: false,
+    Protocoltype: '',
+    CitaId: 0
+  };
+  frontbody: ListDermatomaFrontal = {
+    id: 0,
+    DFC1: false,
+    DFC21: false,
+    DFC22: false,
+    DFC3: false,
+    DFC4: false,
+    DFC51: false,
+    DFC52: false,
+    DFC61: false,
+    DFC62: false,
+    DFC71: false,
+    DFC72: false,
+    DFC81: false,
+    DFC82: false,
+    DFD101: false,
+    DFD102: false,
+    DFD103: false,
+    DFD2: false,
+    DFD3: false,
+    DFD4: false,
+    DFD5: false,
+    DFD6: false,
+    DFD7: false,
+    DFD8: false,
+    DFD9: false,
+    DFD10: false,
+    DFD11: false,
+    DFD12: false,
+    DFL1: false,
+    DFL21: false,
+    DFL22: false,
+    DFL31: false,
+    DFL32: false,
+    DFL41: false,
+    DFL42: false,
+    DFL51: false,
+    DFL52: false,
+    DFS11: false,
+    DFS12: false,
+    DFS2: false,
+    DFS3: false,
+    CitaId: 0
+  };
+  endbody: ListDermatomaPosterior = {
+    id: 0,
+    DDC2: false,
+    DDC3: false,
+    DDC4: false,
+    DDC51: false,
+    DDC52: false,
+    DDC53: false,
+    DDC61: false,
+    DDC62: false,
+    DDC63: false,
+    DDC71: false,
+    DDC72: false,
+    DDC73: false,
+    DDC81: false,
+    DDC82: false,
+    DDC83: false,
+    DDD101: false,
+    DDD102: false,
+    DDD103: false,
+    DDD2: false,
+    DDD3: false,
+    DDD4: false,
+    DDD5: false,
+    DDD6: false,
+    DDD7: false,
+    DDD8: false,
+    DDD9: false,
+    DDD10: false,
+    DDD11: false,
+    DDD12: false,
+    DDL1: false,
+    DDL2: false,
+    DDL31: false,
+    DDL32: false,
+    DDL41: false,
+    DDL42: false,
+    DDL51: false,
+    DDL52: false,
+    DDL53: false,
+    DDL54: false,
+    DDS11: false,
+    DDS12: false,
+    DDS13: false,
+    DDS14: false,
+    DDS21: false,
+    DDS22: false,
+    DDS3: false,
+    DDS4: false,
+    DDS5: false,
+    CitaId: 0
+  };
   usuario: Admin = {
     id: 0,
     Name: '',
@@ -235,6 +381,9 @@ export class Subproceso2de7Component implements OnInit {
     }
   };
   detailhorario: any = this.horariodetail;
+  resultado: any;
+  resultado2: any;
+  resultado3: any;
   constructor(
     private router: Router,
     private toastr: ToastrService,
@@ -242,6 +391,9 @@ export class Subproceso2de7Component implements OnInit {
     private activatedRoute: ActivatedRoute,
     private clienteService: ClienteService,
     private horarioService: HorarioService,
+    private detallecitaService: DetallecitacitaService,
+    private dermatomafrontalService: DermatomafrontalService,
+    private dermatomaposteriorService: DermatomaposteriorService,
   ) { }
   onOptionsSelected(event) {
     const value = event.target.value;
@@ -369,6 +521,36 @@ export class Subproceso2de7Component implements OnInit {
         this.reserva1 = res;
         const codigoreserva = this.reserva1.id;
         const bandera = this.reserva1.Condition;
+        const parametrito = this.reserva1.Type;
+        if (parametrito === 'medicina del dolor') {
+          this.detallecita.CitaId = codigoreserva;
+          this.frontbody.CitaId = codigoreserva;
+          this.endbody.CitaId = codigoreserva;
+          this.detallecitaService.saveDetalleCita(this.detallecita).subscribe(
+            resdedtallecita => {
+              this.resultado = resdedtallecita;
+              console.log(this.resultado);
+            }, err => {
+              this.toastr.error('Error Api CREATE detalle cita');
+            }
+          );
+          this.dermatomafrontalService.saveDermatomafrontal(this.frontbody).subscribe(
+            resfrontal => {
+              this.resultado2 = resfrontal;
+              console.log(this.resultado2);
+            }, err => {
+              this.toastr.error('Error Api CREATE dermatoma frontal');
+            }
+          );
+          this.dermatomaposteriorService.saveDermatomaposterior(this.endbody).subscribe(
+            resposterior => {
+              this.resultado3 = resposterior;
+              console.log(this.resultado3);
+            }, err => {
+              this.toastr.error('Error Api CREATE dermatoma posterior');
+            }
+          );
+        }
         if (bandera === 'reservado') {
           this.router.navigate(
             [
